fedbf944f7dee44a90471ccda42eaab8547b97de
baptistehuteau <baptiste.huteau974@gmail.com>
2026-03-13 11:40:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
-st.write ("bonjour, comment ça va ?")
-st.write ("au top et toi ?")
+
+st.title("Exemple bouton info")
+
+if st.button("Info" \
+""):
+    st.info("Description : La listeria est un infection microbienne dûe à l ingestion de denrées alimentaires contaminée par la bactérie Listeria monocytogène." / 
+            "Celle-ci se développe à rapidement à température ambiante mais est inhibée lorsqu elle est exposée à des températures faibles comme au frigo. Le seul moyen de détruire la bactérie est de faire cuire l'aliment à haute température au four ou à la poele." /
+            "Symptômes : " /
+"- forme digestive avec douleur abdominale, diharrée et symptôme grippale" /
+"- forme invasive avec infection du sang, ou du système nerveux central. Touche d avantage les personnes fragiles" / 
+"- chez la femme enceinte : pas de conséquences ppour la mère, mais peut provoquer un avortement spontané, accouchement prématuré, ou mort foetale."/
+
+"Que faire en cas dingestion : " /
+"Aller chez le médecin pour un diagnostique"/
+
+"https://www.pasteur.fr/fr/centre-medical/fiches-maladies/listeriose")
